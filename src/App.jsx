c0666948d38bbe401782f11e21ba48c31229d9c1
chintakiransai <kirannaiduchinta@gmail.com>
2023-09-11 15:14:18 +0530

    Added react hot toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 
+import { useEffect } from 'react'
+import { toast } from 'react-hot-toast'
 function App() {
+  useEffect(()=>{
+    toast.success("Success")
+  })
 
   return (
     <h1 className="text-3xl font-bold underline">
-      hello
+      APP
     </h1>
-  )
+    )
 }
 
 export default App
